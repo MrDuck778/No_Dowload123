1245b2007c88891fe595c219463f12912a8b0b2b
MrDuck778 <nelemon228@gmail.com>
2024-12-21 14:53:24 +0800

    Add files via upload

---
+++ CODE/virus.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window['bg'] = '#fafaFA'
+window.title('Вирус')
+window.geometry = ('300x300')
+
+for x in range(100):
+    window = Tk()
+    window.title("вирус")
+    window.geometry("600x600")
+    
+window.mainloop()
+
